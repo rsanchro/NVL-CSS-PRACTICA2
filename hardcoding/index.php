b15dfd2b46702e530d9b40a651a3c8b57eabe90f
rsanchro <ruben.arts@gmail.com>
2019-01-03 20:08:19 +0100

    prueba con hardcoing y juntos

--- hardcoding/index.php
+++ hardcoding/index.php
@@ -7,26 +7,9 @@
 		<meta name="description" content="Ejercicio práctico CSS">
 		<meta name="keywords" content="Ejercicio, Práctica 2CSS, HTML5">
 
-		<style>
-			li{
-			    text-align: center;
-			    font-size: 1.8125rem;
-			    font-weight: 300;
-			    color: #1c2c37;
-			    text-transform: uppercase;
-			    margin-bottom: 32px;
-			    background-color: yellow;
-			}
-
-			ul{
-			       list-style:none;
-			}
-
-			h2{
-				color: aqua;
-				background-color: brown;
-				font-family: "Verdana";
-			}
+		
+			
+			
 
 
 	</style>
@@ -37,7 +20,16 @@
 	EJEMPLO DE TITULO H2 
 	</h2>
 		<ul>
-			<li>Lista Desordenada 1</li>
+			<li style="
+			    text-align: center;
+			    font-size: 1.8125rem;
+			    font-weight: 300;
+			    color: #1c2c37;
+			    text-transform: uppercase;
+			    margin-bottom: 32px;
+			    background-color: green;">
+			    HArd Codding a los Bestia
+			</li>
 			<li> OTra cosa</li>
 			<li> Prueba </li>
 		</ul>
