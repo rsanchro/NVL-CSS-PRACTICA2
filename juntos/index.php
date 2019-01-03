59438c4a8c4ab0c8a07a58a356263fe89bd7a2ee
rsanchro <ruben.arts@gmail.com>
2019-01-03 19:58:16 +0100

    Añadida practica con links juntos

---
+++ juntos/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">  
+	<head>
+		<link rel="stylesheet" href="estilos-separados.css">
+		<title> práctica: NVL-CSS-P3a</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Ejercicio práctico CSS">
+		<meta name="keywords" content="Ejercicio, Práctica 2CSS, HTML5">
+
+		<style>
+			li{
+			    text-align: center;
+			    font-size: 1.8125rem;
+			    font-weight: 300;
+			    color: #1c2c37;
+			    text-transform: uppercase;
+			    margin-bottom: 32px;
+			    background-color: yellow;
+			}
+
+			ul{
+			       list-style:none;
+			}
+
+			h2{
+				color: aqua;
+				background-color: brown;
+				font-family: "Verdana";
+			}
+
+
+	</style>
+	</head>
+	<body>
+	
+	<h2>
+	EJEMPLO DE TITULO H2 
+	</h2>
+		<ul>
+			<li>Lista Desordenada 1</li>
+			<li> OTra cosa</li>
+			<li> Prueba </li>
+		</ul>
+	</body>
+</html>
