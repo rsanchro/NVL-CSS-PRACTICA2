f629584041784f48525a4a96f6b4229eef8ae750
rsanchro <ruben.arts@gmail.com>
2019-01-03 19:51:02 +0100

    Primer commit, ejemplo css separado

---
+++ separados/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">  
+	<head>
+		<link rel="stylesheet" href="estilos-separados.css">
+		<title> práctica: NVL-CSS-P3a</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Ejercicio práctico CSS">
+		<meta name="keywords" content="Ejercicio, Práctica 2CSS, HTML5">
+	</head>
+	<body>
+		<ul>
+			<li>Lista Desordenada 1</li>
+			<li> OTra cosa</li>
+			<li> Prueba </li>
+		</ul>
+	</body>
+</html>
