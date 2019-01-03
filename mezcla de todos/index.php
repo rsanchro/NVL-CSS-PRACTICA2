7a99bb30e11e9b6e15ec1d59ad8785febce60114
rsanchro <ruben.arts@gmail.com>
2019-01-03 20:16:17 +0100

    Mezclando todas las opciones de linkear css al html

---
+++ mezcla de todos/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">  
+	<head>
+		<link rel="stylesheet" href="estilos-separados.css">
+		<title> práctica: NVL-CSS-P3a</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Ejercicio práctico CSS">
+		<meta name="keywords" content="Ejercicio, Práctica 2CSS, HTML5">
+
+		<style>
+			li{
+			    text-align: center;
+			    font-size: 1.8125rem;
+			    font-weight: 300;
+			    color: #1c2c37;
+			    text-transform: uppercase;
+			    margin-bottom: 32px;
+			    background-color: yellow;
+			    border: 6px solid blue;
+			}
+
+			ul{
+			       list-style:none;
+			}
+
+	
+
+	</style>
+	</head>
+	<body>
+	
+	<h2 style="color: olive;
+				font-family: "Arial";">
+	EJEMPLO DE TITULO H2 CON ARIAL 
+	</h2>
+		<ul>
+			<li style="border: 3px solid red;">Lista Desordenada 1</li>
+			<li> OTra cosa</li>
+			<li> Prueba </li>
+		</ul>
+	</body>
+</html>
